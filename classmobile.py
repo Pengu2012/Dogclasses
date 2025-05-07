8a02cc55707575aa97bc0457702bdbaa2b0b0cb9
Pengu2012 <tobechi.ogosi@outlook.com>
2025-05-07 16:53:43 +0100

    Add files via upload

---
+++ classmobile.py
@@ -0,0 +1,29 @@
+class Sunny():
+    Temp = 20
+    sunlevel = 100
+    wind = "pleasent"
+    amountofbirds = 75
+    joy = 90
+
+class Sunandcloudy():
+    Temp = 15
+    sunlevel = 50
+    wind = "annoying"
+    amountofbirds = 36
+    joy = 69
+
+class Cloudy():
+    Temp = 10
+    sunlevel = 0
+    wind = "difficult"
+    amountofbirds = 15
+    joy = 30
+
+
+wheatherisgreat = Sunny()
+wheathercouldbebetter = Sunandcloudy()
+Whereisthsun = Cloudy()
+
+print(wheatherisgreat.sunlevel)
+print(wheathercouldbebetter.sunlevel)
+print(Whereisthsun.sunlevel)
